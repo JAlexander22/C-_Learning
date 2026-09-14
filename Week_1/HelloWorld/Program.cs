a75c8064b64c2e8cf9887639e5a63984fee67d2a
JAlexander22 <jordan-alexander99@hotmail.co.uk>
2026-09-14 12:34:05 +0100

    Week_1 HelloWorld

---
+++ Week_1/HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.WriteLine("Hello, World!");
+
+// WriteLine and Write: WriteLine creates a new line
+
+Console.WriteLine("This is your first line.");
+Console.Write("This is ");
+Console.Write("your second ");
+Console.Write("line.");
