9b859eed2f89ae0c7f0abe7cbf55913d2525cc31
JAlexander22 <jordan-alexander99@hotmail.co.uk>
2026-09-16 00:07:52 +0100

    Variables learn done

--- Week_1/Variables/Program.cs
+++ Week_1/Variables/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine('b');
+// Single quotes chrate a character literal , Double quote creates a string data type
+
+// Console.WriteLine('Hello World!');
+// The above code will produce CS1012: Too many characters in character literal
+
+Console.WriteLine(123);
+
+// Float add F at the end 
+Console.WriteLine(0.25F);
+
+// double data type. Means decimal number without a literal suffix (F)
+Console.WriteLine(2.625);
+
+// add M literal suffix to use a decimal data type
+Console.WriteLine(12.39816m);
+
+//Boolean evaluates to true or false
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+// string firstName; 
+// firstName = "Jordan";
+// Console.WriteLine(firstName);
+
+
+// string lastName = "Williams";
+// Console.WriteLine(lastName);
+
+string name = "bob";
+int num = 3;
+float tempFloat = 32.4f;
+
+
+Console.Write(" Hello, " + name + " You have " + num + " messages in your inbox. The temperature is ");
+Console.Write(tempFloat + " celsius");
